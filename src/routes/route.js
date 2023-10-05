aa9442eb22fcd7383f4f7b2b46d7ffde9f52e95d
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-10-05 13:55:54 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,4 +8,8 @@ router.get("/blog-search",analytics,(req,res)=>{
     return res.status(200).send({status:true,data:req.expectedData});
 })
 
+router.all("/*",(req,res)=>{
+    return res.json("Api is running but Path is Invalid")
+})
+
 module.exports=router;
